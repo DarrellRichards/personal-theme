195c8e5ce561ddde64f4fe0b8d49b5c0226502d0
Darrell Richards <hello@darrellrichards.com>
2016-01-06 16:36:13 -0500

    Added Styles for homepage and header.

--- header.php
+++ header.php
@@ -8,3 +8,29 @@
 	<link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/css/main.css">
 </head>
 <body>
+
+<nav id="sidemenu">
+  <ul>
+    <li><a href="#">About</a></li>
+    <li><a href="#">Work</a></li>
+    <li><a href="#">Blog</a></li>
+    <li><a href="#">Contact</a></li>
+  </ul>
+</nav>
+
+<div id="intro_content">
+  <div class="container">
+      <div id="right_side_menu">
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+  	<div id="intro_wrapper">
+  		<!-- <h1>Hello There</h1> -->
+  		<h1>My name is Darrell Richards Jr</h1>
+  		<hr>
+  		<p>I'm a Web Developer located in Roanoke,Virginia</p>
+  		<a href="#nav" class="intro_button"><i class="fa fa-arrow-down fa-1x"></i></a>
+  	</div>
+  </div>
+</div>
