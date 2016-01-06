c8c882155a834b01d70782064db2b1fd0a3c0549
Darrell Richards <hello@darrellrichards.com>
2016-01-06 18:04:19 -0500

    Added font-awesome && Custom Fonts.

--- header.php
+++ header.php
@@ -3,9 +3,12 @@
 <head>
 	<meta charset="UTF-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-  	<meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
 	<title><?php bloginfo('title'); ?></title>
 	<link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/css/main.css">
+  <link href='https://fonts.googleapis.com/css?family=Raleway:400,700,300,100' rel='stylesheet' type='text/css'>
+  <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,300,700,100' rel='stylesheet' type='text/css'>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
 </head>
 <body>
 
