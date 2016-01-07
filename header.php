8d82181980baa1038957c9ac794bcd8a3ed15c2f
Darrell Richards <hello@darrellrichards.com>
2016-01-07 15:54:04 -0500

    Added styles and changed up the menu again.

--- header.php
+++ header.php
@@ -11,49 +11,27 @@
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
 </head>
 <body>
-<div id="sidemenu">
-  <div id="logo">
-    <a href="/"><h3>Darrell Richards</h3></a>
-  </div>
-  <div id="mobile_menu">
-    <span></span>
-    <span></span>
-    <span></span>
-  </div>
-  <?php 
-$defaults = array(
-  'theme_location'  => '',
-  'menu'            => '',
-  'container'       => '',
-  'container_class' => '',
-  'container_id'    => '',
-  'menu_class'      => '',
-  'menu_id'         => '',
-  'echo'            => true,
-  'fallback_cb'     => 'wp_page_menu',
-  'before'          => '',
-  'after'           => '',
-  'link_before'     => '',
-  'link_after'      => '',
-  'items_wrap'      => '<ul id="%1$s" class="%2$s">%3$s</ul>',
-  'depth'           => 0,
-  'walker'          => ''
-);
 
-wp_nav_menu( $defaults );
+<nav class="navbar navbar-default">
+  <div class="container">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="#">Brand</a>
+    </div>
 
-?>
-</div>
-
-
-<!-- 
-<nav id="sidemenu">
-  <ul>
-    <li><a href="#">About</a></li>
-    <li><a href="#">Work</a></li>
-    <li><a href="#">Blog</a></li>
-    <li><a href="#">Contact</a></li>
-  </ul>
-</nav> -->
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="#">Link</a></li>
+      </ul>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
 
 
