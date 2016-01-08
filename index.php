5c0aee801d81da1f008f5a81db80928142de5b6d
Darrell Richards <hello@darrellrichards.com>
2016-01-08 16:15:37 -0500

    Added Blog Page, Updated Styles and content

--- index.php
+++ index.php
@@ -1,10 +1,22 @@
 <?php get_header(); ?>
 <div id="page_wrapper">
-	<h2 class="headline"><?php the_title(); ?></h2>
+	<h2 class="headline">Blog</h2>
 	<div class="container">
 		<div class="page_content">
-			<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-				<p><?php the_content();	 ?></p>
+			<div class="row">
+				<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+					<div class="col-md-8">
+						<div class="post">
+							<h4><?php the_title(); ?></h4>
+							<p><?php the_content();	 ?></p>
+						</div>
+					</div>
+					<div class="col-md-4">
+						<div class="post">
+							Sidebar Coming Soon
+						</div>
+					</div>
+			</div>
 			<?php endwhile; else : ?>
 				<p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
 			<?php endif; ?>
