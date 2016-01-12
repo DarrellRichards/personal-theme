d5539d66d365ee1219eb42c274df4cc0edf4c52e
Darrell Richards <hello@darrellrichards.com>
2016-01-11 20:58:23 -0500

    Added the services loop from services page.

--- front-page.php
+++ front-page.php
@@ -106,9 +106,49 @@
 	<h2 class="headline">Services</h2>
 	<div class="container">
 		<div class="row">
-			<div class="col-md-12">
-				<p>Coming Soon!</p>
+			<?php 
+				$services = array('pagename' => 'services' );
+				$the_query = new WP_Query( $services );
+
+				// The Loop
+				if ( $the_query->have_posts() ) {
+					while ( $the_query->have_posts() ) {
+						$the_query->the_post();
+						
+			?>
+			<?php 
+				if( have_rows('projects') ):
+
+						// loop through the rows of data
+				    while ( have_rows('projects') ) : the_row();
+						$image = get_sub_field('project_main_image');
+						$title = get_sub_field('project_title');
+						$project_link = get_sub_field('project_link');
+			?>
+			<div class="col-md-4">
+				<a href="<?php echo $project_link; ?>">
+					<div class="project">
+						<img src="<?php echo $image; ?>" alt="">
+						<span class="text-content"><span><?php echo $title; ?></span></span>
+					</div>
+				</a>
 			</div>
+
+		<?php
+					endwhile;
+
+						else :
+
+   						 echo "Sorry nothing found";
+
+						endif;
+					}
+				} else {
+					// no posts found
+				}
+				/* Restore original Post Data */
+				wp_reset_postdata();
+			?>
 		</div>
 	</div>
 </div>
